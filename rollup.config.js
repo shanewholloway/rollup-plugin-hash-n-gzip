87b034a136c02146e757556789b286252f563964
Shane Holloway <shane.holloway@ieee.org>
2018-08-23 16:04:30 -0600

    Finished removing JSY build tools. (rename)

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import pkg from './package.json'
 const external = ['crypto', 'fs', 'path', 'util', 'zlib']
 
 export default {
-	input: 'code/index.jsy',
+	input: 'code/index.js',
 	output: [
 		{file: pkg.module, format: 'es'},
 		{file: pkg.main, format: 'cjs'}],
